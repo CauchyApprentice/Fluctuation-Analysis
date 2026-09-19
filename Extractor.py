d18061bcd72c1a1aa2e3b137a03294c39a04a0ff
Raphael Sturm <schlangenkopf@gmx.de>
2026-09-19 10:02:07 +0200

    resolution works

--- Extractor.py
+++ Extractor.py
@@ -4,30 +4,31 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import uproot
 from Settings import Setting, Settings, parameter
-from SimTool import Run
+from SimTool import Run, sim
 from scipy.stats import norm
 
 class Extractor:
     def __init__(self):
         pass
 
-    def apply_resolution(self, energy: list[float], data: list[float]) -> list:
+    def apply_resolution(self, energy: list[float], data: list[float], exp_res: float) -> list:
         h = np.mean(np.diff(energy)) #bin width
-        bin_center = lambda i: energy[i] + h/2
+        energy_edges = energy + [max(energy)+h]
+        bin_center = lambda i: energy_edges[i] + h/2
         smeared = [0.0 for _ in data] #copying data; initializing smeared list
-        sigma = lambda i: parameter[Setting.exp_resolution]
+        sigma = lambda i: exp_res
         for j in range(len(data)):
             weights = (
-                norm.cdf(energy[1:], loc=bin_center(j), scale=sigma(j))
+                norm.cdf(energy_edges[1:], loc=bin_center(j), scale=sigma(j))
                 -
-                norm.cdf(energy[:-1], loc=bin_center(j), scale=sigma(j))
+                norm.cdf(energy_edges[:-1], loc=bin_center(j), scale=sigma(j))
             )
-            smeared += data[:-1][j] * weights
+            smeared += data[j] * weights
         return smeared
                 
 
     def pop_EJ(self, run: Run, *, spin_spacing = 1):
-        energy_bin = parameter[Setting.fluct_bin]
+        energy_bins = parameter[Setting.fluct_bin]
         tree = run.root_tree
         JI_int = tree["JI_int"]
         ExI = tree["ExI"]
@@ -39,9 +40,7 @@ class Extractor:
         spin_min = JI_int.min()
         spin_max = JI_int.max()
         spin_axis = np.arange(spin_min, spin_max + 2*spin_spacing, spin_spacing) #2*spin_spacing works even though 1* should work no?
-        energy_min = 0
-        energy_max = ExI.max() #get the global simulation E_max... whatever that is
-        energy_axis = np.linspace(energy_min, energy_max, energy_bin + 1)
+        energy_axis = np.array(sim.pop.ex_energy_list(Settings.Q_76Ga, energy_bins))
         return np.histogram2d(JI_int, ExI, bins=[spin_axis, energy_axis])
 
     def get_fluct_data_spin(self, run: Run, *, plot: bool = True) -> tuple[np.ndarray, dict[int, np.ndarray]]:
@@ -53,20 +52,18 @@ class Extractor:
             plt.title("EJ counts")
             plt.xlabel("J[hbar]")
             plt.ylabel("Ex[MeV]")
-        print(energy_edges.shape)
         energy = 1/2 * (energy_edges[1:] + energy_edges[:-1])
-        print(energy.shape)
         fluct_data_dict = {}
         spins = spin_edges[:-1]
         for k in range(len(spins)):
             spin = spins[k]
-            fluct_data_dict[int(spin)] = counts[k]
-        return (energy, fluct_data_dict)
+            fluct_data_dict[int(spin)] = counts[k].tolist()
+        return (energy.tolist(), fluct_data_dict)
 
-    def spectrum_smeared(self, run: Run, *, plot: bool = True) -> tuple[list, list]:
+    def spectrum_smeared(self, run: Run, *, exp_res: float = 0.001, plot: bool = True) -> tuple[list, list]:
         energy, fluct_data_dict = self.get_fluct_data_spin(run, plot=plot)
         data = fluct_data_dict[-1]
-        return energy, self.apply_resolution(energy, data)
+        return energy, self.apply_resolution(energy, data, exp_res)
         
 
 extract = Extractor()
